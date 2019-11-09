288ed5aaa0a5eea4284da9f718f8ee2ea43e0fca
alex <lu77al@gmail.com>
2019-11-09 19:48:25 +0200

    almost all instructions

--- main.cpp
+++ main.cpp
@@ -322,6 +322,293 @@ static uint8_t cmd_lds_sts(bool process)
 }
 
 //----------------------------------------------------------------------
+static uint8_t cmd_ld_st_plus(bool process)
+{
+    uint16_t cmd = code[pc];
+    if( (cmd & 0xFC07) != 0x9001)
+        return 0;
+    if( process )
+    {
+        uint8_t reg = F16(cmd, 4, 5);
+        if( BIT(cmd, 9) )
+        {
+            if( BIT(cmd, 3) )
+                sprintf(line[pc].text, "st\tY+,r%d", reg);
+            else
+                sprintf(line[pc].text, "st\tZ+,r%d", reg);
+        }
+        else
+        {
+            if( BIT(cmd, 3) )
+                sprintf(line[pc].text, "ld\tr%d,Y+", reg);
+            else
+                sprintf(line[pc].text, "ld\tr%d,Z+", reg);
+        }
+    }
+    return 1;
+}
+
+//----------------------------------------------------------------------
+static uint8_t cmd_ld_st_minus(bool process)
+{
+    uint16_t cmd = code[pc];
+    if( (cmd & 0xFC07) != 0x9002)
+        return 0;
+    if( process )
+    {
+        uint8_t reg = F16(cmd, 4, 5);
+        if( BIT(cmd, 9) )
+        {
+            if( BIT(cmd, 3) )
+                sprintf(line[pc].text, "st\t-Y,r%d", reg);
+            else
+                sprintf(line[pc].text, "st\t-Z,r%d", reg);
+        }
+        else
+        {
+            if( BIT(cmd, 3) )
+                sprintf(line[pc].text, "ld\tr%d,-Y", reg);
+            else
+                sprintf(line[pc].text, "ld\tr%d,-Z", reg);
+        }
+    }
+    return 1;
+}
+
+//----------------------------------------------------------------------
+static uint8_t cmd_e_lpm(bool process)
+{
+    uint16_t cmd = code[pc];
+    if( (cmd & 0xFE0D) != 0x9004)
+        return 0;
+    if( process )
+    {
+        uint8_t reg = F16(cmd, 4, 5);
+        if( BIT(cmd, 1) )
+            sprintf(line[pc].text, "elpm\tr%d,Z", reg);
+        else
+            sprintf(line[pc].text, "lpm\tr%d,Z", reg);
+    }
+    return 1;
+}
+
+//----------------------------------------------------------------------
+static uint8_t cmd_e_lpm_plus(bool process)
+{
+    uint16_t cmd = code[pc];
+    if( (cmd & 0xFE0D) != 0x9005)
+        return 0;
+    if( process )
+    {
+        uint8_t reg = F16(cmd, 4, 5);
+        if( BIT(cmd, 1) )
+            sprintf(line[pc].text, "elpm\tr%d,Z+", reg);
+        else
+            sprintf(line[pc].text, "lpm\tr%d,Z+", reg);
+    }
+    return 1;
+}
+
+//----------------------------------------------------------------------
+static uint8_t cmd_ld_st_x(bool process)
+{
+    uint16_t cmd = code[pc];
+    if( (cmd & 0xFC0C) != 0x900C)
+        return 0;
+    uint8_t type = F16(cmd, 0, 2);
+    if( type == 3 )
+        return 0;
+    if( process )
+    {
+        uint8_t reg = F16(cmd, 4, 5);
+        if( BIT(cmd, 9) )
+            switch( type ) {
+            case 0:
+                sprintf(line[pc].text, "st\tX,r%d", reg);
+                break;
+            case 1:
+                sprintf(line[pc].text, "st\tX+,r%d", reg);
+                break;
+            default:
+                sprintf(line[pc].text, "st\t-X,r%d", reg);
+            }
+        else
+            switch( type ) {
+            case 0:
+                sprintf(line[pc].text, "ld\tr%d,X", reg);
+                break;
+            case 1:
+                sprintf(line[pc].text, "ld\tr%d,X+", reg);
+                break;
+            default:
+                sprintf(line[pc].text, "ld\tr%d,-X", reg);
+            }
+    }
+    return 1;
+}
+
+//----------------------------------------------------------------------
+static uint8_t cmd_push_pop(bool process)
+{
+    uint16_t cmd = code[pc];
+    if( (cmd & 0xFC0F) != 0x900F)
+        return 0;
+    if( process )
+    {
+        uint8_t reg = F16(cmd, 4, 5);
+        if( BIT(cmd, 9) )
+            sprintf(line[pc].text, "push\tr%d", reg);
+        else
+            sprintf(line[pc].text, "pop\tr%d", reg);
+    }
+    return 1;
+}
+
+//----------------------------------------------------------------------
+static uint8_t cmd_one_operand(bool process)
+{
+    static const char oo_instr[8][5] =
+        { "com", "neg", "swap", "inc", "", "asr", "lsr", "ror" };
+    uint16_t cmd = code[pc];
+    if( (cmd & 0xFE08) != 0x9400)
+        return 0;
+    uint8_t type = F16(cmd, 0, 3);
+    if( type == 4 )
+        return 0;
+    if( process )
+    {
+        uint8_t reg = F16(cmd, 4, 5);
+        sprintf(line[pc].text, "%s\tr%d", oo_instr[type], reg);
+    }
+    return 1;
+}
+
+//----------------------------------------------------------------------
+static uint8_t cmd_sex_clx(bool process)
+{
+    static const char *status_bit = "cznvshti";
+    uint16_t cmd = code[pc];
+    if( (cmd & 0xFF0F) != 0x9408)
+        return 0;
+    if( process )
+    {
+        uint8_t bit = F16(cmd, 4, 3);
+        if( BIT(cmd, 7) )
+            sprintf(line[pc].text, "cl%c", status_bit[bit]);
+        else
+            sprintf(line[pc].text, "se%c", status_bit[bit]);
+    }
+    return 1;
+}
+
+//----------------------------------------------------------------------
+static uint8_t cmd_ret_reti(bool process)
+{
+    uint16_t cmd = code[pc];
+    if( (cmd & 0xFFEF) != 0x9508 )
+        return 0;
+    if( process )
+    {
+        if( BIT(cmd, 4) )
+            sprintf(line[pc].text, "reti");
+        else
+            sprintf(line[pc].text, "ret");
+        pc--;
+    }
+    return 1;
+}
+
+//----------------------------------------------------------------------
+static uint8_t cmd_misc(bool process)
+{
+    static const char instr[7][8] =
+        { "sleep", "break", "wdr", "lpm", "elpm", "spm", "spm Z+" };
+    static const uint8_t instr_code[7] =
+        {   0x8,     0x9,     0xA,  0xC,    0xD,   0xE,    0xF    };
+    uint16_t cmd = code[pc];
+    if( (cmd & 0xFF0F) != 0x9508)
+        return 0;
+    uint8_t type = F16(cmd, 4, 4);
+    uint8_t i = 0;
+    for(; (i < 7) && (instr_code[i] != type); i++ );
+    if( i >= 7 )
+        return 0;
+    if( process )
+        sprintf(line[pc].text, instr[i]);
+    return 1;
+}
+
+//----------------------------------------------------------------------
+static uint8_t cmd_ijmp_icall(bool process)
+{
+    uint16_t cmd = code[pc];
+    if( (cmd & 0xFEEF) != 0x9409)
+        return 0;
+    if( process )
+    {
+        if( BIT(cmd, 8) )
+            sprintf(line[pc].text, "icall");
+        else
+            sprintf(line[pc].text, "ijmp");
+    }
+    return 1;
+}
+
+//----------------------------------------------------------------------
+static uint8_t cmd_dec(bool process)
+{
+    uint16_t cmd = code[pc];
+    if( (cmd & 0xFE0F) != 0x940A)
+        return 0;
+    if( process )
+    {
+        uint8_t reg = F16(cmd, 4, 5);
+        sprintf(line[pc].text, "dec\tr%d", reg);
+    }
+    return 1;
+}
+
+//----------------------------------------------------------------------
+static uint8_t cmd_jmp_call(bool process)
+{
+    uint16_t cmd = code[pc];
+    if( (cmd & 0xFE0C) != 0x940C)
+        return 0;
+    if( process )
+    {
+        uint16_t addr = code[pc+1];
+        line[addr].pointed = true;
+        line[pc+1].visited = true;
+        if( BIT(cmd, 1) )
+            sprintf(line[pc].text, "call\tL%X", addr);
+        else
+            sprintf(line[pc].text, "jmp\tL%X", addr);
+        pc = addr - 2;
+    }
+    return 2;
+}
+
+//----------------------------------------------------------------------
+static uint8_t cmd_adiw_subiw(bool process)
+{
+    static const char reg_apir[4][16] =
+        { "W", "XH:XL", "YH:YL", "ZH:ZL" };
+    uint16_t cmd = code[pc];
+    if( (cmd & 0xFE00) != 0x9600)
+        return 0;
+    if( process )
+    {
+        uint16_t pair = F16(cmd, 4, 2);
+        uint8_t val = (F16(cmd, 6, 2) << 4) + F16(cmd, 0, 4);
+        if( BIT(cmd, 8) )
+            sprintf(line[pc].text, "sbiw\t%s,%d\t// %02X", reg_apir[pair], val, val);
+        else
+            sprintf(line[pc].text, "adiw\t%s,%d\t// %02X", reg_apir[pair], val, val);
+    }
+    return 1;
+}
+
+//----------------------------------------------------------------------
 static uint8_t cmd_rjmp_rcall(bool process)
 {
     uint16_t cmd = code[pc];
@@ -359,35 +646,6 @@ static uint8_t cmd_ldi(bool process)
     return 1;
 }
 
-//----------------------------------------------------------------------
-static uint8_t cmd_push_pop(bool process)
-{
-    uint16_t cmd = code[pc];
-    if( (cmd & 0xFC0F) != 0x900F)
-        return 0;
-    if( process )
-    {
-        uint8_t reg = F16(cmd, 4, 5);
-        if( BIT(cmd, 9) )
-            sprintf(line[pc].text, "push\tr%d", reg);
-        else
-            sprintf(line[pc].text, "pop\tr%d", reg);
-    }
-    return 1;
-}
-
-//----------------------------------------------------------------------
-static uint8_t cmd_reti(bool process)
-{
-    if( code[pc] != 0x9518)
-        return 0;
-    if( process )
-    {
-        sprintf(line[pc].text, "reti");
-        pc--;
-    }
-    return 1;
-}
 
 //----------------------------------------------------------------------
 static COMMAND_t command[] = {
@@ -407,11 +665,23 @@ static COMMAND_t command[] = {
     cmd_andi,
     cmd_ldd_std,
     cmd_lds_sts,
+    cmd_ld_st_plus,
+    cmd_ld_st_minus,
+    cmd_e_lpm,
+    cmd_e_lpm_plus,
+    cmd_ld_st_x,
+    cmd_push_pop,
+    cmd_one_operand,
+    cmd_sex_clx,
+    cmd_ret_reti,
+    cmd_misc,
+    cmd_ijmp_icall,
+    cmd_dec,
+    cmd_jmp_call,
+    cmd_adiw_subiw,
 
     cmd_rjmp_rcall,
-    cmd_ldi,
-    cmd_push_pop,
-    cmd_reti
+    cmd_ldi
 };
 #define COMMAND_COUNT (int(sizeof(command)/sizeof(COMMAND_t)))
 
